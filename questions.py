725ead89bbc27063dc673a5bc5aab6a20c15f773
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-28 17:57:21 +0600

    Exercises

---
+++ questions.py
@@ -0,0 +1,8 @@
+f = open('out.txt','a')
+
+f.write('\n'+ input('What is your name and surname?'))
+f.write('\n'+ input('How old are you?'))
+f.write('\n'+ input('Where are you from?'))
+f.write('\n'+ input('What did you get on midterm exam?'))
+
+f.close()
