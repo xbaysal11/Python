ddbd031bbf93bab8db19af28b435e3cc06fcb9fb
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-28 17:57:56 +0600

    Exercises

---
+++ radiationExposure.py
@@ -0,0 +1,18 @@
+def f(x):
+    
+    import math
+    return 10*math.e**(math.log(0.5)/5.27 * x)
+
+def radiationExposure(start, stop, step):
+
+    radiation = 0
+    count = start
+    while count < stop:        
+        radiation += step * f(count)
+        count += step
+    return radiation
+    
+start = input()
+stop = input()
+step = input()
+print radiationExposure(start, stop, step)
