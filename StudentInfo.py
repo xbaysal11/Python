aff8396918d422c6b2be13c113351994c2314c83
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-28 17:25:56 +0600

    Exercises

---
+++ StudentInfo.py
@@ -0,0 +1,7 @@
+def student_info(name, surname, age ):
+    return student_info( name , surname, age )
+name = str(input())
+surname = str(input())
+age = int(input())
+    
+print(name, surname, 'is', str(age), 'years old.')
