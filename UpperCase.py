219958409949007ffd22c9c0846be6d4e21a92b0
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-28 17:29:59 +0600

    Exercises

---
+++ UpperCase.py
@@ -0,0 +1,6 @@
+isLower = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+let = input()
+if let in isLower:
+    print('True')
+else:
+    print('False')
