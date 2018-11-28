43a9506e71ea38f2ad54da9c7a51adc594e455ff
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-28 16:58:42 +0600

    Exercises

---
+++ Division.py
@@ -0,0 +1,7 @@
+Dividend = int(input("Enter a dividend: "))
+Divisor  = int(input("Enter a divisor: "))
+if Divisor == 0:
+    print("Incorrect input!")
+else: 
+	Quotient = Dividend / Divisor
+	print("Quotient is: ", str(Quotient))
