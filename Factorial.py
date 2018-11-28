ddb769bd597e79cc53285e984f6376312eada3ef
Baisalbek Daniiarov <baisalbek.daniiarov@gmail.com>
2018-11-28 17:26:47 +0600

    Exercises

--- Factorial.py
+++ Factorial.py
@@ -1,10 +1,9 @@
-n = int(input('Enter a number: '))
-if n < 0:
-    print('Negative integer.')
-else:    
-    i = 1
-    fact = 1
-    while i <= n:
-        fact *= i
-        i += 1
-    print('Factorial of ' + str(n) + ' is ' + str(fact))
+def factorial(n):
+    num = 1
+    while n >= 1:
+        num = num * n
+        n = n - 1
+    return num
+
+n = input()
+print (factorial(n))
